8dd33d3ad53f55dc8c7c4c054f14a14735dd6373
Kaid Wong <kaid@kaid.me>
2011-03-30 16:32:39 +0800

    fix init.sh

--- tools/init.sh
+++ tools/init.sh
@@ -1,22 +1,22 @@
 if [ -d ~/.vim_kaid ];then
   rm -rf ~/.vim_kaid
   echo "Cloning Kaid's dotvim..."
-  /usr/bin/env git clone git@github.com:kaid/dotvim.git .vim_kaid 2>/dev/null 2>&1 && r=1 || r=0
+  /usr/bin/env git clone git@github.com:kaid/dotvim.git .vim_kaid 2>&1 && _rslt=1 || _rslt=0
 fi
 
-if [ $r==0 ];then
+if [ $rslt==0 ];then
   /usr/bin/env git clone git://github.com/kaid/dotvim.git .vim_kaid
 fi
 
 echo "Cloned!"
 
 function _backup() {
-  d=$(date +"%s")
+  _ds=$(date +"%s")
   echo "Backing up existing $1?(y/n):"
   while read yon; do
     case $yon in
-      y)echo "Backing up it to $1_$d...";
-        mv $1 $1_$d;
+      y)echo "Backing up it to $1_$_ds...";
+        mv $1 $1_$_ds;
         echo "Backuped!";break;;
       n)echo "Deleting $1..."
         rm -rf $1;
